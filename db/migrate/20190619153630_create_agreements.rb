e4c760383c9381d703404fec8302cc6d2117c558
Valerie Woolard <valerie.woolard@gmail.com>
2019-06-19 12:21:40 -0400

    Frontend

--- db/migrate/20190619153630_create_agreements.rb
+++ db/migrate/20190619153630_create_agreements.rb
@@ -1,8 +1,8 @@
 class CreateAgreements < ActiveRecord::Migration[5.2]
   def change
     create_table :agreements do |t|
-      t.Country :country1
-      t.Country :country2
+      t.integer :country1
+      t.integer :country2
       t.text :type
 
       t.timestamps
